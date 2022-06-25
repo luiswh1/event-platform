f18426c41c5942bfd0a81b8e08b90eb643353f6c
Luis Weber <luisw.h@live.com>
2022-06-25 00:17:30 -0300

    background image added

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,6 +3,7 @@ import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
+import codeMockUpImg from '../assets/code-mockup.png';
 
 export function Subscribe() {
   const navigate = useNavigate()
@@ -67,7 +68,7 @@ export function Subscribe() {
         </div>
       </div>
 
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={codeMockUpImg} className="mt-10" alt="" />
     </div>
   );
 }
